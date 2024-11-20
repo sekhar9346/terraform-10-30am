62f94b80a5a288935c6eda50a25ac266ea41264e
veera <veerababu.narni@gmail.com>
2024-11-20 11:33:30 +0530

    day-5-updates

--- day-5-backend_config/backend.tf
+++ day-5-backend_config/backend.tf
@@ -1,8 +1,9 @@
 
+# Run first terraform-10-30am/day-4-resources_for_backend_s3_dynamodb to create reources 
 # This backend configuration instructs Terraform to store its state in an S3 bucket.
  terraform {
 backend "s3" {
-    bucket         = "testdevnareshit"  # Name of the S3 bucket where the state will be stored.
+    bucket         = "nareshmulticloud"  # Name of the S3 bucket where the state will be stored.
     region         =  "us-east-1"
     key            = "terraform.tfstate" # Path within the bucket where the state will be read/written.
     dynamodb_table = "terraform-state-lock-dynamo" # DynamoDB table used for state locking, note: first run day-4-bckend resources then day-5-backend config
--- day-5-backend_config/terraform.tfvars
+++ day-5-backend_config/terraform.tfvars
@@ -1,3 +1,3 @@
-ami = "ami-0440d3b780d96b29d"
- instance_type ="t2.micro"
-#  key_name = "linux"
+ami = "ami-012967cc5a8c9f891"
+instance_type ="t2.micro"
+key_name = "newkey"
