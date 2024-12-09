d61347c8e3879294d20d31fa74d25d0637ebc40c
sekhar9346 <anakalasekhar004@gmail.com>
2024-12-09 16:12:25 +0530

    Update main.tf

--- day-2-basic-ec2/main.tf
+++ day-2-basic-ec2/main.tf
@@ -3,7 +3,7 @@
 resource "aws_instance" "dev" {
     ami = var.ami
     instance_type = var.instance_type
-    key_name = var.key_name
+    key_name = keypair
     tags = {
       Name = "dev-ec3"
     }
